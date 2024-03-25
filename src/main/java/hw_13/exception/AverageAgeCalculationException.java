65faeffb8671fcbe4c7d5e24c540960ac51900c0
superPlatypus <kirillpashovkin@gmail.com>
2024-03-25 16:10:16 +0300

    HW-13

---
+++ src/main/java/hw_13/exception/AverageAgeCalculationException.java
@@ -0,0 +1,7 @@
+package hw_13.exception;
+
+public class AverageAgeCalculationException extends IllegalArgumentException {
+    public AverageAgeCalculationException(String message) {
+        super(message);
+    }
+}
